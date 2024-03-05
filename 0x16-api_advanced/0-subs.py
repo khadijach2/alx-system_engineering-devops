5673015deea261b262e6773903d8755f3fe1c84d
khadijach2 <charef2.khadija@gmail.com>
2024-03-05 13:02:27 -0800

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,19 +17,13 @@ def number_of_subscribers(subreddit):
     Returns:
         int: number of subscribers
     """
-    base_url = 'https://www.reddit.com/r/'
 
-    url = '{}{}/about.json'.format(base_url, subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    results = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if results.status_code == 200:
-        return results.json()['data']['subscribers']
-    return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+        return results.get("subscribers")
